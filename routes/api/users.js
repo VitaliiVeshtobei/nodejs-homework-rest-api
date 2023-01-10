bc390378615d118e6185273d029deb4f980a6ac2
Vitalii <99075525+VitaliiVeshtobei@users.noreply.github.com>
2023-01-10 21:31:17 +0200

    Hw05 avatars (#4)
    hw05-merge

--- routes/api/users.js
+++ routes/api/users.js
@@ -7,6 +7,7 @@ const {
   validationUserSubscription,
 } = require("../../middlewares/middlewareValidation");
 const { auth } = require("../../middlewares/auth");
+const { upload } = require("../../middlewares/upload");
 
 const {
   signup,
@@ -14,12 +15,14 @@ const {
   getCurrent,
   logout,
   updateSubscription,
+  updateAvatar,
 } = require("../../controllers/users");
 
 router.post("/signup", validationUser, signup);
 router.post("/login", validationUser, login);
 router.post("/current", auth, validationUser, getCurrent);
 router.get("/logout", auth, logout);
-router.patch("/:userID", validationUserSubscription, updateSubscription);
+// router.patch("/:userID", validationUserSubscription, updateSubscription);
+router.patch("/avatars", auth, upload.single("avatar"), updateAvatar);
 
 module.exports = router;
